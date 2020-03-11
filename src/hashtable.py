3935b7cdfdd97dd9b8f3242a0799849515f5a3d9
scottwmwork <scottwmwork@gmail.com>
2020-03-11 09:00:53 -0700

    added linked list traversal on remove()

--- src/hashtable.py
+++ src/hashtable.py
@@ -58,10 +58,18 @@ class HashTable:
         if self.storage[index] is not None:
             next_linked_pair = self.storage[index]
             while next_linked_pair is not None:
+
+                # Overwriting 
+                if next_linked_pair.key == key:
+                    next_linked_pair.value = value
+                    return
+
+                # Looking for empty next
                 if next_linked_pair.next is None:
                     next_linked_pair.next = LinkedPair(key, value)
-                    print("Inserted:", next_linked_pair.next.value)
                     return
+                
+                # Increment to next node
                 else:
                     next_linked_pair = next_linked_pair.next
 
@@ -83,15 +91,26 @@ class HashTable:
 
         index = self._hash_mod(key)
 
-        if self.storage[key] is not None:
-            self.storage[index] = None 
-        else:
-            print("Warning: Key not found")
+        if self.storage[index].key == key:
+            self.storage[index] = None
+            return 
+    
+        next_linked_pair = self.storage[index]
+        while next_linked_pair is not None:
+            if next_linked_pair.key == key:
+                next_linked_pair = None
+                return 
+            else:
+                next_linked_pair = next_linked_pair.next
+
+        # if key not found in linked list
+        print("Error: key is not found")
+
 
     def retrieve(self, key):
         '''
         Retrieve the value stored with the given key.
-
+ 
         Returns None if the key is not found.
 
         Fill this in.
@@ -127,6 +146,11 @@ class HashTable:
         self.storage = [None] * self.capacity
         for item in old_storage:
             self.insert(item.key, item.value)
+            
+            # insert next linked pairs if they exist
+            while item.next is not None:
+                item = item.next
+                self.insert(item.key, item.value)
 
 if __name__ == "__main__":
     ht = HashTable(2)
@@ -146,18 +170,16 @@ if __name__ == "__main__":
     print(ht.retrieve("line_3"))
 
     # Test resizing
-    # old_capacity = len(ht.storage)
-    # ht.resize()
-    # new_capacity = len(ht.storage)
-
-    # print(f"\nResized from {old_capacity} to {new_capacity}.\n")
+    old_capacity = len(ht.storage)
+    ht.resize()
+    new_capacity = len(ht.storage)
 
     # Test if data intact after resizing
-    # print("1.")
-    # print(ht.retrieve("line_1"))
-    # print("2.")
-    # print(ht.retrieve("line_2"))
-    # print("3.")
-    # print(ht.retrieve("line_3"))
+    print("1.")
+    print(ht.retrieve("line_1"))
+    print("2.")
+    print(ht.retrieve("line_2"))
+    print("3.")
+    print(ht.retrieve("line_3"))
 
     # print("")
